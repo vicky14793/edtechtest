243539e56cc40b017cfcbd4a9617848a2260253a
Vicky Nagar <vicky.n14793@gmail.com>
2023-01-30 11:16:44 +0530

    third

---
+++ server/knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  client: "pg",
+  connection: {
+    host: "localhost",
+    user: "postgres",
+    password: "qwertii123",
+    database: "testDB",
+  },
+  migrations: {
+    directory: __dirname + "/db/migrations",
+  },
+  seeds: {
+    directory: __dirname + "/db/seeds",
+  },
+};
